ff6561e5e8593eb794510c64bd516207bf5d0fd9
Boffins03 <boffins03@gmail.com>
2024-06-01 11:26:22 +0530

    comment added

--- Learning/Bro code/15.list,tuple.py
+++ Learning/Bro code/15.list,tuple.py
@@ -1,12 +1,12 @@
-import random
+import random       # import random module
 
-tuple=(4,5,6,3,2,1)
-list=[1,2,3,4,5]
-a={'a':1,'b':2,'c':3}
-b=[[3,4,2],655,3,6,3,2]
-print(b[0][2])
-c={"my","name","is","Huzaifa"}
-c.pop()
+tuple=(4,5,6,3,2,1) # creating a tuple
+list=[1,2,3,4,5]    # creating a list                
+a={'a':1,'b':2,'c':3} # creating dictionary 
+b=[[3,4,2],655,3,6,3,2] # creating 2D list
+print(b[0][2]) # print 2 from inner list
+c={"my","name","is","Huzaifa"} # creating set
+c.pop() # pop last element
 print(c)
 for i in c:
     print(i)
@@ -16,12 +16,12 @@ print(a.items())
 # print(round(abs(float(input("enter a number")))))
 
 a="name"
-def function_name(Name,Class):
+def function_name(Name,Class): # Creating a function with two passing argument
     print(f'My name is {Name} and I am in class {Class}')
 
-function_name("Huzaifa","B.CA")
+function_name("Huzaifa","B.CA") # calling fuction
 
-def arg(*num):
+def arg(*num): # function in which you can pass as many as argument  
     sum=0
     for i in num:
         sum += i
@@ -30,7 +30,7 @@ def arg(*num):
 b=arg(1,2,3,4,5)    
 print(b)
 
-def karg(**k):
+def karg(**k): # function taking keyword argument 
     print(f"My name is {k['name']}. I live in {k['city']}. I am {k['age']} years old.")
 
 karg(name="Huzaifa",city="Shamli",age=20)
@@ -48,11 +48,11 @@ print(list)
 # print(n,m)
 # print(type(n),type(m))
 
-S=input("Enter a string")
+S=input("Enter a string :") 
 a=len(S)
 s=[]
 for i in S:
-   s.append(i)
+   s.append(i) # appending every single element from string into list
    
-s.sort()
+s.sort() # sorting list
 print(s) 
